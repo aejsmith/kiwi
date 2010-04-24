cfb078a261d1f3b4f9795f3150e7fd1aa1f2175c
Alex Smith <alex@alex-smith.me.uk>
2010-04-24 11:16:35 +0100

    Upgraded toolchain to Binutils 2.20.1 and GCC 4.5.0.

--- utilities/toolchain/binutils/__init__.py
+++ utilities/toolchain/binutils/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 Alex Smith
+# Copyright (C) 2009-2010 Alex Smith
 #
 # Kiwi is open source software, released under the terms of the Non-Profit
 # Open Software License 3.0. You should have received a copy of the
@@ -16,7 +16,7 @@ from utilities.toolchain.component import ToolchainComponent
 
 class BinutilsComponent(ToolchainComponent):
 	name = 'binutils'
-	version = '2.19.1'
+	version = '2.20.1'
 	source = [
 		'http://ftp.gnu.org/gnu/binutils/binutils-' + version + '.tar.bz2',
 	]
