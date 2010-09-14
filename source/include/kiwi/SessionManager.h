be7ac8520e09e1eafc9cfb99ffbac67182defd90
Alex Smith <alex@alex-smith.me.uk>
2010-09-14 21:14:47 +0100

    Added SessionManager class to libkiwi-service that allows services to interact with the session manager (the in-progress window server uses this to maintain a separate state for each session).

---
+++ source/include/kiwi/SessionManager.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010 Alex Smith
+ *
+ * Kiwi is open source software, released under the terms of the Non-Profit
+ * Open Software License 3.0. You should have received a copy of the
+ * licensing information along with the source code distribution. If you
+ * have not received a copy of the license, please refer to the Kiwi
+ * project website.
+ *
+ * Please note that if you modify this file, the license requires you to
+ * ADD your name to the list of contributors. This boilerplate is not the
+ * license itself; please refer to the copy of the license you have received
+ * for complete terms.
+ */
+
+/**
+ * @file
+ * @brief		Session manager interface.
+ */
+
+#ifndef __KIWI_SESSIONMANAGER_H
+#define __KIWI_SESSIONMANAGER_H
+
+#include <kernel/types.h>
+
+#include <kiwi/Object.h>
+#include <kiwi/Signal.h>
+
+namespace kiwi {
+
+/** Class to interface with the session manager. */
+class SessionManager : public Object {
+public:
+	SessionManager();
+
+	status_t CreateSession(session_id_t &id);
+	status_t SwitchSession(session_id_t id);
+	
+	Signal<session_id_t> OnCreateSession;
+	Signal<session_id_t> OnDestroySession;
+	Signal<session_id_t, session_id_t> OnSwitchSession;
+private:
+	void *m_conn;			/**< Connection implementation. */
+};
+
+}
+
+#endif /* __KIWI_SESSIONMANAGER_H */
---
+++ source/libraries/kiwi/service/SessionManager.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2010 Alex Smith
+ *
+ * Kiwi is open source software, released under the terms of the Non-Profit
+ * Open Software License 3.0. You should have received a copy of the
+ * licensing information along with the source code distribution. If you
+ * have not received a copy of the license, please refer to the Kiwi
+ * project website.
+ *
+ * Please note that if you modify this file, the license requires you to
+ * ADD your name to the list of contributors. This boilerplate is not the
+ * license itself; please refer to the copy of the license you have received
+ * for complete terms.
+ */
+
+/**
+ * @file
+ * @brief		Session manager interface.
+ */
+
+#include <kiwi/SessionManager.h>
+#include "org.kiwi.SessionManager.h"
+
+using namespace kiwi;
+using namespace org::kiwi::SessionManager;
+
+/** Macro to get the session manager connection. */
+#define GET_CONN()	(reinterpret_cast<ServerConnection *>(m_conn))
+
+/** Connect to the session manager. */
+SessionManager::SessionManager() {
+	m_conn = new ServerConnection();
+	GET_CONN()->OnCreateSession.Connect(OnCreateSession);
+	GET_CONN()->OnDestroySession.Connect(OnDestroySession);
+	GET_CONN()->OnSwitchSession.Connect(OnSwitchSession);
+}
+
+/** Create a new session.
+ * @param id		Where to store ID of session.
+ * @return		Status code describing result of the operation. */
+status_t SessionManager::CreateSession(session_id_t &id) {
+	return GET_CONN()->CreateSession(id);
+}
+
+/** Switch to a different session.
+ * @param id		ID of session to switch to.
+ * @return		Status code describing result of the operation. */
+status_t SessionManager::SwitchSession(session_id_t id) {
+	return GET_CONN()->SwitchSession(id);
+}
