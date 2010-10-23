6019aebeedfd1f25ef8f668f25700775fbdf7b01
Alex Smith <alex@alex-smith.me.uk>
2010-10-23 10:32:07 +0100

    Temporarily disabled build of everything in userspace, added a test program.

---
+++ source/binaries/test/main.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2010 Alex Smith
+ *
+ * Kiwi is open source software, released under the terms of the Non-Profit
+ * Open Software License 3.0. You should have received a copy of the
+ * licensing information along with the source code distribution. If you
+ * have not received a copy of the license, please refer to the Kiwi
+ * project website.
+ *
+ * Please note that if you modify this file, the license requires you to
+ * ADD your name to the list of contributors. This boilerplate is not the
+ * license itself; please refer to the copy of the license you have received
+ * for complete terms.
+ */
+
+/**
+ * @file
+ * @brief		Test app.
+ */
+
+#include <kernel/device.h>
+#include <kernel/process.h>
+
+extern void _start(process_args_t *args);
+
+static void message(const char *buf) {
+	size_t i;
+
+	for(i = 0; buf[i]; i++) {}
+	device_write(1, buf, i, 0, NULL);
+}
+
+void _start(process_args_t *args) {
+	message("Hello World\n");
+	while(1);
+}
