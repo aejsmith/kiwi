89240e8290c5686efb05a062176c951d708f2e43
Alex Smith <alex@alex-smith.me.uk>
2012-02-08 11:20:42 +0000

    Cleaned up kmalloc() and friends.

--- source/kernel/mm/malloc.c
+++ source/kernel/mm/malloc.c
@@ -30,7 +30,7 @@
  * allocation larger than 64K is requested, then the allocation will use the
  * kernel heap directly.
  *
- * Allocations are tracked using an alloc_btag_t structure, which is placed
+ * Allocations are tracked using an alloc_tag_t structure, which is placed
  * before the allocation in memory. It tracks the size of the allocation and
  * the cache it came from. If the allocation came from the heap, then the
  * cache pointer will be NULL.
@@ -46,10 +46,10 @@
 #include <kernel.h>
 
 /** Information structure prepended to allocations. */
-typedef struct alloc_btag {
-	unative_t size;			/**< Size of the allocation. */
+typedef struct alloc_tag {
+	size_t size;			/**< Size of the allocation. */
 	slab_cache_t *cache;		/**< Pointer to cache for allocation. */
-} alloc_btag_t;
+} alloc_tag_t;
 
 /** Cache settings. */
 #define KMALLOC_CACHE_MIN	5	/**< Minimum cache size (2^5  == 32). */
@@ -63,41 +63,37 @@ static slab_cache_t *kmalloc_caches[KMALLOC_CACHE_MAX - KMALLOC_CACHE_MIN + 1];
  * @param kmflag	Allocation flags.
  * @return		Pointer to block on success, NULL on failure. */
 void *kmalloc(size_t size, int kmflag) {
-	size_t total = size + sizeof(alloc_btag_t), idx;
-	alloc_btag_t *addr;
+	size_t total = size + sizeof(alloc_tag_t), idx;
+	alloc_tag_t *addr;
 
 	/* Use the slab caches where possible. */
 	if(total <= (1 << KMALLOC_CACHE_MAX)) {
 		/* If exactly a power-of-two, then highbit(total) will work,
 		 * else we want the next size up. Remember that the highbit
 		 * function returns (log2(n) + 1). */
-		idx = ((total & (total - 1)) == 0) ? highbit(total) - 1 : highbit(total);
+		idx = (IS_POW2(total)) ? highbit(total) - 1 : highbit(total);
 		if(idx < KMALLOC_CACHE_MIN) {
 			idx = KMALLOC_CACHE_MIN;
 		}
 		idx -= KMALLOC_CACHE_MIN;
 
 		addr = slab_cache_alloc(kmalloc_caches[idx], kmflag);
-		if(addr == NULL) {
+		if(unlikely(!addr)) {
 			return NULL;
 		}
 
-		addr->size = size;
 		addr->cache = kmalloc_caches[idx];
-		return &addr[1];
-	}
-
-	/* Fall back on the kernel heap. */
-	addr = heap_alloc(ROUND_UP(total, PAGE_SIZE), (kmflag & MM_FLAG_MASK) & ~MM_FATAL);
-	if(addr == NULL) {
-		if(kmflag & MM_FATAL) {
-			fatal("Could not perform mandatory allocation (%zu bytes)", size);
+	} else {
+		/* Fall back on the kernel heap. */
+		addr = heap_alloc(ROUND_UP(total, PAGE_SIZE), kmflag & MM_FLAG_MASK);
+		if(unlikely(!addr)) {
+			return NULL;
 		}
-		return NULL;
+
+		addr->cache = NULL;
 	}
 
 	addr->size = size;
-	addr->cache = NULL;
 	return &addr[1];
 }
 
@@ -130,23 +126,26 @@ void *kcalloc(size_t nmemb, size_t size, int kmflag) {
  * @return		Pointer to block on success, NULL on failure.
  */
 void *krealloc(void *addr, size_t size, int kmflag) {
-	alloc_btag_t *btag = (alloc_btag_t *)(addr - sizeof(alloc_btag_t));
+	alloc_tag_t *tag;
 	void *ret;
 
-	if(addr == NULL) {
+	if(!addr) {
 		return kmalloc(size, kmflag);
-	} else if(btag->size == size) {
+	}
+
+	tag = (alloc_tag_t *)((char *)addr - sizeof(alloc_tag_t));
+	if(tag->size == size) {
 		return addr;
 	}
 
 	/* Make a new allocation. */
 	ret = kmalloc(size, kmflag);
-	if(ret == NULL) {
+	if(!ret) {
 		return ret;
 	}
 
 	/* Copy the block data using the smallest of the two sizes. */
-	memcpy(ret, addr, MIN(btag->size, size));
+	memcpy(ret, addr, MIN(tag->size, size));
 
 	/* Free the old allocation. */
 	kfree(addr);
@@ -162,20 +161,20 @@ void *krealloc(void *addr, size_t size, int kmflag) {
  * @param addr		Address to free. If NULL, nothing is done.
  */
 void kfree(void *addr) {
-	alloc_btag_t *btag;
+	alloc_tag_t *tag;
 
 	if(addr) {
-		btag = (alloc_btag_t *)(addr - sizeof(alloc_btag_t));
+		tag = (alloc_tag_t *)((char *)addr - sizeof(alloc_tag_t));
 
 		/* If the cache pointer is not set, assume the allocation came
 		 * directly from the heap. */
-		if(btag->cache == NULL) {
-			heap_free(btag, ROUND_UP(btag->size + sizeof(alloc_btag_t), PAGE_SIZE));
+		if(!tag->cache) {
+			heap_free(tag, ROUND_UP(tag->size + sizeof(alloc_tag_t), PAGE_SIZE));
 			return;
 		}
 
 		/* Free to the cache it came from. */
-		slab_cache_free(btag->cache, btag);
+		slab_cache_free(tag->cache, tag);
 	}
 }
 
@@ -184,17 +183,12 @@ __init_text void malloc_init(void) {
 	char name[SLAB_NAME_MAX];
 	size_t i, size;
 
-	/* Create all of the cache structures. */
 	for(i = 0; i < ARRAY_SIZE(kmalloc_caches); i++) {
 		size = (1 << (i + KMALLOC_CACHE_MIN));
-
 		snprintf(name, SLAB_NAME_MAX, "kmalloc_%zu", size);
 		name[SLAB_NAME_MAX - 1] = 0;
 
 		kmalloc_caches[i] = slab_cache_create(name, size, 0, NULL, NULL,
-		                                      NULL, 0, 0);
-		if(kmalloc_caches[i] == NULL) {
-			fatal("Could not create malloc cache: %s", name);
-		}
+		                                      NULL, 0, MM_FATAL);
 	}
 }
