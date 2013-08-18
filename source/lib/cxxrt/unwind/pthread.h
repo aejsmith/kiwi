3dc2f6c27e98076fe236fdf690ba4d7ae1383256
Alex Smith <alex@alex-smith.me.uk>
2013-08-18 12:00:38 +0100

    Build libunwind into libcxxrt for the _Unwind_* exception handling functions.

---
+++ source/lib/cxxrt/unwind/pthread.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2013 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file
+ * @brief		POSIX threads.
+ */
+
+#ifndef __PTHREAD_H
+#define __PTHREAD_H
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int32_t pthread_mutex_t;
+typedef int32_t pthread_mutexattr_t;
+
+#define PTHREAD_MUTEX_INITIALIZER 0
+
+extern int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
+extern int pthread_mutex_lock(pthread_mutex_t *);
+extern int pthread_mutex_unlock(pthread_mutex_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PTHREAD_H */
