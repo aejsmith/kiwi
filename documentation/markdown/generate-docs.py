e404219d13997c36ebe93fcb34cd0a63d4fea367
Alex Smith <alex@alex-smith.me.uk>
2013-10-19 17:13:52 +0100

    generate-docs.py: skip any file that doesn't look like Markdown
    Don't generate output for any file that doesn't look like a Markdown-
    formatted file.
    
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

--- documentation/markdown/generate-docs.py
+++ documentation/markdown/generate-docs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2009-2011 Alex Smith
+# Copyright (C) 2009-2013 Alex Smith
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -15,47 +15,46 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
-"""
-Markdown documentation generator
-"""
-
 import sys, os, glob
 from markdown2 import markdown
 from jinja2 import Environment, FileSystemLoader
 
 def main():
-	if len(sys.argv) != 3:
-		print "Usage: %s <docs dir> <output dir>" % (sys.argv[0])
-		return 1
+    if len(sys.argv) != 3:
+        print "Usage: %s <docs dir> <output dir>" % (sys.argv[0])
+        return 1
 
-	# Get a list of documents and parse their content.
-	documents = []
-	for doc in glob.glob(os.path.join(sys.argv[1], '*.txt')):
-		# Assume that the first line is the title, and that there are
-		# two lines after it before the actual content.
-		f = open(doc, 'r')
-		title = f.readline().strip()
-		f.readline()
-		f.readline()
+    # Get a list of documents and parse their content.
+    documents = []
+    for doc in glob.glob(os.path.join(sys.argv[1], '*.txt')):
+        # Assume that the first line is the title, and that there are two lines
+        # after it before the actual content. Skip any document that does not
+        # have a H1 at the start.
+        f = open(doc, 'r')
+        title = f.readline().strip()
+        underline = f.readline().strip()
+        if len(title) != len(underline) or underline[0] != '=':
+            continue
+        f.readline()
 
-		# Parse the document.
-		html = markdown(f.read()).encode(sys.stdout.encoding or "utf-8", 'xmlcharrefreplace')
-		f.close()
+        # Parse the document.
+        html = markdown(f.read()).encode(sys.stdout.encoding or "utf-8", 'xmlcharrefreplace')
+        f.close()
 
-		# Work out the output file name.
-		name = os.path.splitext(os.path.basename(doc))[0] + '.html'
+        # Work out the output file name.
+        name = os.path.splitext(os.path.basename(doc))[0] + '.html'
 
-		# Add it to the list.
-		documents.append((name, title, html))
+        # Add it to the list.
+        documents.append((name, title, html))
 
-	# Create the template loader.
-	tpl = Environment(loader=FileSystemLoader(os.path.join(sys.argv[1], 'markdown'))).get_template('template.html')
+    # Create the template loader.
+    tpl = Environment(loader=FileSystemLoader(os.path.join(sys.argv[1], 'markdown'))).get_template('template.html')
 
-	# For each document, write the template.
-	for doc in documents:
-		f = open(os.path.join(sys.argv[2], doc[0]), 'w')
-		f.write(tpl.render(name=doc[0], title=doc[1], content=doc[2], docs=documents))
-		f.close()
+    # For each document, write the template.
+    for doc in documents:
+        f = open(os.path.join(sys.argv[2], doc[0]), 'w')
+        f.write(tpl.render(name=doc[0], title=doc[1], content=doc[2], docs=documents))
+        f.close()
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
