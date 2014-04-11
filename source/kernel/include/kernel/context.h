e779f442edd14c2a27054fc8bdbd6bedf477ab31
Alex Smith <alex@alex-smith.me.uk>
2014-04-11 08:16:04 +0100

    Add missing kernel/context.h header
    Signed-off-by: Alex Smith <alex@alex-smith.me.uk>

---
+++ source/kernel/include/kernel/context.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file
+ * @brief		CPU context structure.
+ */
+
+#ifndef __KERNEL_CONTEXT_H
+#define __KERNEL_CONTEXT_H
+
+#include <kernel/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __x86_64__
+
+/** Structure describing a CPU execution context. */
+typedef struct cpu_context {
+	unsigned long rax;
+	unsigned long rbx;
+	unsigned long rcx;
+	unsigned long rdx;
+	unsigned long rdi;
+	unsigned long rsi;
+	unsigned long rbp;
+	unsigned long r8;
+	unsigned long r9;
+	unsigned long r10;
+	unsigned long r11;
+	unsigned long r12;
+	unsigned long r13;
+	unsigned long r14;
+	unsigned long r15;
+	unsigned long rip;
+	unsigned long rflags;
+	unsigned long rsp;
+
+	// TODO: FPU state...
+} cpu_context_t;
+
+#else
+
+#error "No cpu_context_t defined for this architecture"
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __KERNEL_CONTEXT_H_ */
