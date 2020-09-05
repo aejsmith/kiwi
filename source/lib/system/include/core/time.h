3f313fe5e0376dc1ada155efdc84dd6a31b3930c
Alex Smith <alex@alex-smith.me.uk>
2020-09-05 13:46:56 +0100

    Add time unit conversion functions to core library

---
+++ source/lib/system/include/core/time.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2009-2020 Alex Smith
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file
+ * @brief               Time functions.
+ */
+
+#ifndef __CORE_TIME_H
+#define __CORE_TIME_H
+
+#include <kernel/time.h>
+
+/** Convert seconds to nanoseconds.
+ * @param secs          Seconds value to convert.
+ * @return              Equivalent time in nanoseconds. */
+static inline nstime_t core_secs_to_nsecs(nstime_t secs) {
+    return secs * 1000000000;
+}
+
+/** Convert milliseconds to nanoseconds.
+ * @param msecs         Milliseconds value to convert.
+ * @return              Equivalent time in nanoseconds. */
+static inline nstime_t core_msecs_to_nsecs(nstime_t msecs) {
+    return msecs * 1000000;
+}
+
+/** Convert microseconds to nanoseconds.
+ * @param usecs         Microseconds value to convert.
+ * @return              Equivalent time in nanoseconds. */
+static inline nstime_t core_usecs_to_nsecs(nstime_t usecs) {
+    return usecs * 1000;
+}
+
+/** Convert nanoseconds to seconds.
+ * @param nsecs         Nanoseconds value to convert.
+ * @return              Equivalent time in seconds. */
+static inline nstime_t core_nsecs_to_secs(nstime_t nsecs) {
+    return nsecs / 1000000000;
+}
+
+/** Convert nanoseconds to milliseconds.
+ * @param nsecs         Nanoseconds value to convert.
+ * @return              Equivalent time in milliseconds. */
+static inline nstime_t core_nsecs_to_msecs(nstime_t nsecs) {
+    return nsecs / 1000000;
+}
+
+/** Convert nanoseconds to microseconds.
+ * @param nsecs         Nanoseconds value to convert.
+ * @return              Equivalent time in microseconds. */
+static inline nstime_t core_nsecs_to_usecs(nstime_t nsecs) {
+    return nsecs / 1000;
+}
+
+#endif /* __CORE_TIME_H */
