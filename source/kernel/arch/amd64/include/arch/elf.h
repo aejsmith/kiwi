681498db13a31e5ecf81b5a3896b43d8f512a50c
Alex Smith <alex@alex-smith.me.uk>
2021-03-15 19:49:45 +0000

    Add AArch64 ELF definitions

--- source/kernel/arch/amd64/include/arch/elf.h
+++ source/kernel/arch/amd64/include/arch/elf.h
@@ -22,15 +22,15 @@
 #pragma once
 
 /** Definitions of ELF machine type, endianness, etc. */
-#define ELF_MACHINE ELF_EM_X86_64   /**< ELF machine (x86_64). */
-#define ELF_CLASS   ELFCLASS64      /**< ELF class (64-bit). */
-#define ELF_ENDIAN  ELFDATA2LSB     /**< ELF endianness (little-endian). */
+#define ELF_MACHINE ELF_EM_X86_64
+#define ELF_CLASS   ELFCLASS64
+#define ELF_ENDIAN  ELFDATA2LSB
 
 /** Type definitions to select the right structures/types to use. */
-typedef Elf64_Ehdr elf_ehdr_t;      /**< ELF executable header. */
-typedef Elf64_Phdr elf_phdr_t;      /**< ELF program header. */
-typedef Elf64_Shdr elf_shdr_t;      /**< ELF section header. */
-typedef Elf64_Sym  elf_sym_t;       /**< ELF symbol structure. */
-typedef Elf64_Addr elf_addr_t;      /**< ELF address type. */
-typedef Elf64_Rel  elf_rel_t;       /**< ELF REL type. */
-typedef Elf64_Rela elf_rela_t;      /**< ELF RELA type. */
+typedef Elf64_Ehdr elf_ehdr_t;
+typedef Elf64_Phdr elf_phdr_t;
+typedef Elf64_Shdr elf_shdr_t;
+typedef Elf64_Sym  elf_sym_t;
+typedef Elf64_Addr elf_addr_t;
+typedef Elf64_Rel  elf_rel_t;
+typedef Elf64_Rela elf_rela_t;
