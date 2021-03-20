0ab3be8c3dbfb38e2ec09bd7f1eb86b94d915223
Alex Smith <alex@alex-smith.me.uk>
2021-03-20 15:05:01 +0000

    Implement arch_page_init() for ARM64

--- source/kernel/arch/arm64/page.c
+++ source/kernel/arch/arm64/page.c
@@ -21,10 +21,27 @@
 
 #include <mm/page.h>
 
+#include <kboot.h>
 #include <kernel.h>
+
+#define A4G 0x100000000ull
 
 /** Add memory ranges to the physical memory manager. */
 __init_text void arch_page_init(void) {
-    /* TODO: This should be arch-specific I think? */
-    fatal_todo();
+    kboot_tag_foreach(KBOOT_TAG_MEMORY, kboot_tag_memory_t, range) {
+        phys_ptr_t end = range->start + range->size;
+
+        /* Determine which free list pages in the range should be put in. If
+         * necessary, split into multiple ranges. */
+        if (range->start < A4G) {
+            if (end <= A4G) {
+                page_add_memory_range(range->start, end, PAGE_FREE_LIST_BELOW4G);
+            } else {
+                page_add_memory_range(range->start, A4G, PAGE_FREE_LIST_BELOW4G);
+                page_add_memory_range(A4G, end, PAGE_FREE_LIST_ABOVE4G);
+            }
+        } else {
+            page_add_memory_range(range->start, end, PAGE_FREE_LIST_ABOVE4G);
+        }
+    }
 }
